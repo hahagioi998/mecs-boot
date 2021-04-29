174285a5ef5f417a2695f65d697b303cc085a1ea
QiuJa <759502610@qq.com>
2021-04-29 20:51:25 +0800

    1

--- mecs.sql
+++ mecs.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 80018
  Source Host           : localhost:3306
- Source Schema         : mecs
+ Source Schema         : boot-mecs
 
  Target Server Type    : MySQL
  Target Server Version : 80018
